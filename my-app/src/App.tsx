df12b7956d652a44b19aeb266561df0bdd4dc485
Shobhan-Pakhira <87083191+Shobhan-Pakhira@users.noreply.github.com>
2023-03-13 17:32:48 +0530

    Initial Stage

---
+++ my-app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DigitalMarketing from './views/DigitalMarketing';
+
+function App() {
+  return (
+    <DigitalMarketing/>
+  );
+}
+
+export default App;
---
+++ my-app/src/views/DigitalMarketing.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DigitalMarketing = () => {
+  return (
+    <div className='container'>
+        <div className='col-6'>side content</div>
+        <div className='col-6'>Image</div>
+    </div>
+  )
+}
+
+export default DigitalMarketing
